716117ee8b389d05003545b955a70bd165284b7b
Simon de la Rouviere <simon@delarouviere.com>
2022-10-04 17:14:16 -0400

    reshuffled order of the page to put minting on top

--- packages/react-app/src/components/IntroPage.js
+++ packages/react-app/src/components/IntroPage.js
@@ -25,7 +25,8 @@ function IntroPage(props) {
 
     const offlineHTML = <Fragment>
     [In order to mint an NFT, you need to  have a web3/Ethereum-enabled browser and connect it (see top right of the page). Please download
-      the <a href="https://metamask.io">MetaMask Chrome extension</a> or open in an Ethereum-compatible browser.]
+      the <a href="https://metamask.io">MetaMask Chrome extension</a> or open in an Ethereum-compatible browser.] <br />
+      <br />
     </Fragment>;
 
     // Not being used in the interface but keeping it in here for future
@@ -76,12 +77,6 @@ function IntroPage(props) {
           if(unix >= startDateUnix) { disabled = false; }
 
           let claimHTML = <Fragment>
-            <Button type="primary" size={"medium"} disabled={true} loading={props.minting} onClick={claim}>
-                Claim a Random Capsule 
-              </Button>
-              <br />
-              <br />
-            You are not eligible for a free claim. <br />
           </Fragment>
 
           // verify is address is in the tree
@@ -92,8 +87,9 @@ function IntroPage(props) {
 
           if(inTree) {
             claimHTML = <Fragment>
+              <br />
               <Button type="primary" size={"medium"} disabled={disabled} loading={props.minting} onClick={claim}>
-                Claim a Random Capsule
+                Claim a Random Capsule (Free)
               </Button>
               <br />
               <br />
@@ -102,8 +98,9 @@ function IntroPage(props) {
 
               if(props.alreadyClaimed == true) {
                 claimHTML = <Fragment>
+                  <br />
               <Button type="primary" size={"medium"} disabled={false} loading={props.minting} onClick={claim}>
-                Claim a Random Capsule
+                Claim a Random Capsule (Free)
               </Button>
               <br />
               <br />
@@ -118,26 +115,10 @@ function IntroPage(props) {
             disabledSeedMint = disabled;
           }
           mintButton = <Button type="primary" size={"medium"} disabled={disabledSeedMint} loading={props.minting} onClick={mintCustomNFT}>
-            Mint Custom Capsule with Seed: {displayFromSeedForm.getFieldValue("seed")}
+            Mint Custom Capsule with Seed (0.074 ETH): {displayFromSeedForm.getFieldValue("seed")}
           </Button>
 
           const newMintHTML = <Fragment>
-            <h3>Custom Capsules: {props.seedPrice} ETH per mint.</h3> Available to mint, with no supply limit, from {startDateString} until {endDateString}.<br />
-            <br />
-            To mint a custom capsule, you must choose a seed. The capsule that will be generated is defined by this seed and the address used to mint it. Thus, it remains a unique capsule to you! 
-            Each custom capsule is generated from this combined seed to produce 29 unique variables that generates a unique capsule. There are 53 experiences to draw from.<br />
-            <br />
-            Preview the capsule by playing with the number in the form below.
-            If you are happy with the result, you can mint it.<br />
-            <br />
-            <h3> Random Capsules: {props.randomPrice} ETH per mint.</h3> Available to mint and claim, with no supply limit, from {startDateString} until {endDateString}.<br />
-            <br />
-            Can't decide what to mint? Want to be surprised? Mint a random capsule. Along with the 62 experiences, there's a 1/3 chance for each experience that is generated to be from a rare set of experiences (33 experiences)! 
-            The rare experiences can only be generated from random mints.
-            A rare experience is denoted by a striped line instead of a solid line.
-            <br />
-            <br />
-            <h2> [] MINT </h2>
             {displaySection}
 
             <Form layout="inline" size="small"  form={displayFromSeedForm} name="control-hooks" onFinish={displayFromSeed} onValuesChange={seedChanged}>
@@ -163,15 +144,16 @@ function IntroPage(props) {
             <br />
             <br />
             <Button type="primary" size="medium" disabled={disabled} loading={props.minting} onClick={mintRandomNFT}>
-              Mint a Random Capsule
+              Mint a Random Capsule (0.022 ETH)
             </Button>
-            <br />
             <br />
             {claimHTML}
             <br />
             By minting, you agree to the <a href="https://github.com/Untitled-Frontier/tlatc/blob/master/TOS_PP.pdf">Terms of Service</a>.
             <br />
             <br />
+
+            
           </Fragment>
 
           setMintSection(newMintHTML);
@@ -226,23 +208,36 @@ function IntroPage(props) {
 
     return (
         <div className="App" style={{textAlign:"justify"}}> 
+        <br />
+        From <b>{startDateString} until {endDateString}</b>, fans can mint or claim on-chain, generative art capsules from the Logged Universe story, "Upstream Glitches" (a limited time only)! Details below.
+        <br />
+        <br />
+        {/* MINT SECTION */}
+        <div className="section">
+        {mintSection}
+        </div>
+        <h2>[] DETAILS </h2>
         <img src={NFTImg} alt="Capsules of All Our Lives" style={{display:"block", marginLeft:"auto", marginRight: "auto", maxWidth:"100%"}}/> <br />
-        In the story, <a href="https://www.untitledfrontier.studio/blog/logged-universe-4-upstream-glitches">"Upstream Glitches" by Vesta Gheibi</a>, one of the lessons in the simulation's first school of unlearning, Marsa and other uploaded minds, have to capture all the lives they have lived in capsules. 
-        Fans can collect these capsules as memorabilia in the form of on-chain generative art SVG.
+        Each capsule contains 29 (!) different variables that inform and change its appearance. Each unique is thus unique, lending itself to different glitches, shades of colours, and marked experiences.
         <br />
         <br />
-        <video controls src="https://d2ybmb80bbm9ts.cloudfront.net/Dp/Ys/QmdQoiskPF8iqkVMU4rNteqrCbVtioMuA3F8iXXpHoDpYs/nft_q4.mp4" width="100%">
-        Sorry, your browser doesn't support embedded videos.
-        </video>
-        <i>The cover video as an NFT by Untitled Frontier and Dr. Chicken Gristle. <a href="https://foundation.app/@un_frontier/lucs/1">Available on Foundation</a>.</i>
+        <h3>CUSTOM CAPSULES: {props.seedPrice} ETH per mint.</h3> Available to mint, with no supply limit, from {startDateString} until {endDateString}.<br />
+        <br />
+        To mint a custom capsule, you must choose a seed. The capsule that will be generated is defined by this seed and the address used to mint it. Thus, it remains a unique capsule to you! 
+        Each custom capsule is generated from this combined seed to produce 29 unique variables that generates a unique capsule. <b>There are 53 experiences to draw from.</b><br />
+        <br />
+        Preview the capsule by playing with the number in the form below.
+        If you are happy with the result, you can mint it.<br />
+        <br />
+        <h3> RANDOM CAPSULES: {props.randomPrice} ETH per mint.</h3> Available to mint and claim, with no supply limit, from {startDateString} until {endDateString}.<br />
+        <br />
+        Can't decide what to mint? Want to be surprised? Mint a random capsule. Along with the 62 experiences, there's a 1/3 chance for each experience that is generated to be from a rare set of experiences (33 experiences)! 
+        <b> The rare experiences can only be generated from random mints.</b>
+        A rare experience is denoted by a striped line instead of a solid line.
         <br />
         <br />
-        <h2>[] Capsules of All Our Lives</h2>
-        From {startDateString} until {endDateString}, fans can mint or claim on-chain capsules. To be eligible to claim a capsule for free, you would have had to have owned an NFT from all the previous 3 stories combined by {snapshotDate}. Any amount of capsules can be generated during the campaign period of 4 weeks. 
+        To be eligible to claim a capsule for free, you would have had to have owned an NFT from all the previous 3 stories combined by {snapshotDate}. Any amount of capsules can be generated during the campaign period of 4 weeks. 
         After this period, no new ones can be minted or claimed.
-        <br />
-        <br />
-        Each capsule contains 29 different variables that inform and change its appearance. Each unique is thus unique, lending itself to different glitches, shades of colours, and marked experiences.
         <br />
         <br />
         The components that make up the Capsules are licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>. Thus, you are free to use the NFTs as you wish. <a href="https://github.com/Untitled-Frontier/ug">The code is available on Github.</a>
@@ -251,11 +246,18 @@ function IntroPage(props) {
         You can view already minted "Capsules" on <a href="https://opensea.io/collection/capsules-of-all-our-lives" target="_blank">OpenSea</a>.
         <br />
         <br />
-        {/* MINT SECTION */}
-        <div className="section">
-        <h2>[] DETAILS: Custom vs Random Capsules </h2>
-        {mintSection}
-        </div>
+        <h2> [] Upstream Glitches </h2>
+        These capsules are collectible memorabilia from the fourth story in the Logged Universe: <a href="https://www.untitledfrontier.studio/blog/logged-universe-4-upstream-glitches">"Upstream Glitches" by Vesta Gheibi.</a>  
+        <span> In one of the lessons in the simulation's first school of unlearning, Marsa and other uploaded minds, have to capture all the lives they have lived in capsules. </span>
+        <br />
+        <br />
+        <video controls src="https://d2ybmb80bbm9ts.cloudfront.net/Dp/Ys/QmdQoiskPF8iqkVMU4rNteqrCbVtioMuA3F8iXXpHoDpYs/nft_q4.mp4" width="100%">
+        Sorry, your browser doesn't support embedded videos.
+        </video>
+        <i>The cover video as an NFT by Untitled Frontier and Dr. Chicken Gristle and is also <a href="https://foundation.app/@un_frontier/lucs/1">available on Foundation</a>.</i>
+        <br />
+        <br />
+
         <br />
         <br />
         </div>
